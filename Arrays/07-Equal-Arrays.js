bb0d4936cda6c0c3bf86293833b3dba6832a3609
NGNikolay <georgiev.nikolaj@gmail.com>
2022-12-01 20:06:59 +0200

    add solution

---
+++ Arrays/07-Equal-Arrays.js
@@ -0,0 +1,22 @@
+function equalArrays(firstArray, secondArray) {
+
+    let sum = 0;
+    let index = 0;
+
+
+    for (let i = 0; i < firstArray.length; i++) {
+        index = i;
+        if (firstArray[i] == secondArray[i]) {
+            sum += Number(firstArray[i]);
+            if (i == firstArray.length - 1) {
+                let positiveResult = `Arrays are identical. Sum: ${sum}`;
+                console.log(positiveResult);
+            }
+        } else {
+            let negativeResult = `Arrays are not identical. Found difference at ${index} index`;
+            console.log(negativeResult);
+        }
+    }
+
+}
+equalArrays(['10', '20', '30'], ['10', '20', '30']);
