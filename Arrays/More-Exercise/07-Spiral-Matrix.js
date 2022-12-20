6bb0490018d5b7d9681885d7845b083cb1046fdd
NGNikolay <georgiev.nikolaj@gmail.com>
2022-12-20 14:02:03 +0200

    Create 07-Spiral-Matrix.js

---
+++ Arrays/More-Exercise/07-Spiral-Matrix.js
@@ -0,0 +1,44 @@
+function spiralMatrix(rolls, columns) {
+
+    let matrixArray = [];
+
+    for (let i = 0; i < columns; i++) {
+        matrixArray.push([]);
+    }
+
+    let counter = 1;
+    let startRoll = 0;
+    let startColumn = 0;
+    let endRoll = rolls - 1;
+    let endColumn = columns - 1;
+
+    while (startRoll <= endRoll && startColumn <= endColumn) {
+
+        for (let i = startColumn; i <= endColumn; i++) {
+            matrixArray[startRoll][i] = counter;
+            counter++;
+        }
+        startRoll++;
+        for (let i = startRoll; i <= endRoll; i++) {
+            matrixArray[i][endColumn] = counter;
+            counter++;
+        }
+        endColumn--;
+        for (let i = endColumn; i >= startColumn; i--) {
+            matrixArray[endRoll][i] = counter;
+            counter++;
+        }
+        endRoll--;
+        for (let i = endRoll; i >= startRoll; i--) {
+            matrixArray[i][startColumn] = counter;
+            counter++;
+        }
+        startColumn++;
+
+    }
+    for (let i = 0; i < matrixArray.length; i++) {
+        console.log(matrixArray[i].join(' '));
+    }
+
+}
+spiralMatrix(5, 5);
