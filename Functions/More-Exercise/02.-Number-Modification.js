331bc132bd8e872a143846aca94f05e990af5efb
NGNikolay <georgiev.nikolaj@gmail.com>
2023-01-08 12:34:53 +0200

    Create 02.-Number-Modification.js

---
+++ Functions/More-Exercise/02.-Number-Modification.js
@@ -0,0 +1,47 @@
+function numberModification(number) {
+
+    let numberString = String(number);
+    let workArray = [];
+
+    for (const iterator of numberString) {
+        workArray.push(iterator);
+    }
+
+    let averageDivider = (workArray[0] === '-') ? workArray.length - 1 : workArray.length;
+
+    let sumOfDigits = function(array) {
+        let result = 0;
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === '-') {
+                result -= Number(array[i + 1]);
+                i++;
+            } else {
+                result += Number(array[i]);
+            }
+        }
+        return result;
+    }
+
+    averageCheck(sumOfDigits(workArray));
+
+    function averageCheck(number) {
+        if (number <= 5) {
+            workArray.push(9);
+            averageDivider += 1;
+            sumOfDigits(workArray);
+            averageCheck(sumOfDigits(workArray) / averageDivider);
+        } else {
+            if (workArray[0] === '-') {
+                workArray.shift();
+                let result = workArray.join('');
+                return result;
+            } else {
+                let result = workArray.join('');
+                return result;
+            }
+        }
+    }
+
+    console.log(averageCheck(sumOfDigits(workArray)));
+}
+numberModification(101);
